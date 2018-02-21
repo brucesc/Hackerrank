0a4b69b00a6fbefa070954a04b5f8b584995ba8f
Steven Bruce <Brucesc726@gmail.com>
2018-02-20 21:35:41 -0500

    Completed BirtdayCakeCandles, this was a poorly thought out and written problem

---
+++ BirthdayCakeCandles/BirthdayCakeCandles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static int birthdayCakeCandles(int n, int[] ar)
+    {
+        int max = ar[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (ar[i] > max)
+            {
+                max = ar[i];
+            }
+        }
+
+        int counter = 0;
+
+        foreach (int candle in ar)
+        {
+            if (candle == max)
+            {
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] ar_temp = Console.ReadLine().Split(' ');
+        int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+        int result = birthdayCakeCandles(n, ar);
+        Console.WriteLine(result);
+    }
+}
